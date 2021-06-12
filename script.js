a1bca56665c3dcc63117a7802bf9d4cb4282ccf9
Omar Yazbek <rott.artwork@gmail.com>
2021-06-11 17:42:47 -0700

    basic CRUD scheduler using simple submit and and click events to add and delete

---
+++ script.js
@@ -0,0 +1,194 @@
+const form1 = document.getElementById("form1");
+// add event
+form1.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity1 = document.getElementById("activity1");
+  const input1 = document.getElementById("input1");
+  activity1.innerText = input1.value;
+  input1.value = "";
+});
+// delete event
+activity1.addEventListener("click", e => {
+  activity1.innerText = "";
+});
+// Repeat for each form - very simple beginner CRUD logic
+
+const form2 = document.getElementById("form2");
+form2.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity2 = document.getElementById("activity2");
+  const input2 = document.getElementById("input2");
+  activity2.innerText = input2.value;
+  input2.value = "";
+});
+activity2.addEventListener("click", e => {
+  activity2.innerText = "";
+});
+
+const form3 = document.getElementById("form3");
+form3.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity3 = document.getElementById("activity3");
+  const input3 = document.getElementById("input3");
+  activity3.innerText = input3.value;
+  input3.value = "";
+});
+activity3.addEventListener("click", e => {
+  activity3.innerText = "";
+});
+
+const form4 = document.getElementById("form4");
+form4.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity4 = document.getElementById("activity4");
+  const input4 = document.getElementById("input4");
+  activity4.innerText = input4.value;
+  input4.value = "";
+});
+activity4.addEventListener("click", e => {
+  activity4.innerText = "";
+});
+
+const form5 = document.getElementById("form5");
+form5.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity5 = document.getElementById("activity5");
+  const input5 = document.getElementById("input5");
+  activity5.innerText = input5.value;
+  input5.value = "";
+});
+activity5.addEventListener("click", e => {
+  activity5.innerText = "";
+});
+
+const form6 = document.getElementById("form6");
+form1.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity6 = document.getElementById("activity6");
+  const input6 = document.getElementById("input6");
+  activity6.innerText = input6.value;
+  input6.value = "";
+});
+activity6.addEventListener("click", e => {
+  activity6.innerText = "";
+});
+
+const form7 = document.getElementById("form7");
+form7.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity7 = document.getElementById("activity7");
+  const input7 = document.getElementById("input7");
+  activity7.innerText = input7.value;
+  input7.value = "";
+});
+activity7.addEventListener("click", e => {
+  activity7.innerText = "";
+});
+
+const form8 = document.getElementById("form8");
+form8.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity8 = document.getElementById("activity8");
+  const input8 = document.getElementById("input8");
+  activity8.innerText = input8.value;
+  input8.value = "";
+});
+activity8.addEventListener("click", e => {
+  activity8.innerText = "";
+});
+
+const form9 = document.getElementById("form9");
+form9.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity9 = document.getElementById("activity9");
+  const input9 = document.getElementById("input9");
+  activity9.innerText = input9.value;
+  input9.value = "";
+});
+activity9.addEventListener("click", e => {
+  activity9.innerText = "";
+});
+
+const form10 = document.getElementById("form10");
+form10.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity10 = document.getElementById("activity10");
+  const input10 = document.getElementById("input10");
+  activity10.innerText = input10.value;
+  input10.value = "";
+});
+activity10.addEventListener("click", e => {
+  activity10.innerText = "";
+});
+
+const form11 = document.getElementById("form11");
+form11.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity11 = document.getElementById("activity11");
+  const input11 = document.getElementById("input11");
+  activity11.innerText = input11.value;
+  input11.value = "";
+});
+activity11.addEventListener("click", e => {
+  activity11.innerText = "";
+});
+
+const form12 = document.getElementById("form12");
+form12.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity12 = document.getElementById("activity12");
+  const input12 = document.getElementById("input12");
+  activity12.innerText = input12.value;
+  input12.value = "";
+});
+activity12.addEventListener("click", e => {
+  activity12.innerText = "";
+});
+
+const form13 = document.getElementById("form13");
+form13.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity13 = document.getElementById("activity13");
+  const input13 = document.getElementById("input13");
+  activity13.innerText = input13.value;
+  input13.value = "";
+});
+activity13.addEventListener("click", e => {
+  activity13.innerText = "";
+});
+
+const form14 = document.getElementById("form14");
+form14.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity14 = document.getElementById("activity14");
+  const input14 = document.getElementById("input14");
+  activity14.innerText = input14.value;
+  input14.value = "";
+});
+activity14.addEventListener("click", e => {
+  activity14.innerText = "";
+});
+
+const form15 = document.getElementById("form15");
+form15.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity15 = document.getElementById("activity15");
+  const input15 = document.getElementById("input15");
+  activity15.innerText = input15.value;
+  input15.value = "";
+});
+activity15.addEventListener("click", e => {
+  activity15.innerText = "";
+});
+
+const form16 = document.getElementById("form16");
+form16.addEventListener("submit", e => {
+  e.preventDefault();
+  const activity16 = document.getElementById("activity16");
+  const input16 = document.getElementById("input16");
+  activity16.innerText = input16.value;
+  input16.value = "";
+});
+activity16.addEventListener("click", e => {
+  activity16.innerText = "";
+});
